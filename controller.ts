d5b4b4d3399045846f903ee2d49eeb079fd7ad98
A1iyah <aliyah1mesi@gmail.com>
2023-05-26 13:05:54 +0300

    filer-movies

--- controller.ts
+++ controller.ts
@@ -16,37 +16,134 @@ function MovieSearchFiltering(searchFields: string[]) {}
 function aliyaFunction() {}
 
 /////////////////////////////////////////////////////////////////////////////////////
-// - Aliyah Movie Cards - //
-const movieCards = document.querySelector(".movieCards") as HTMLDivElement;
+// - ALIYAH MOVIE CARDS & FILTER - //
 
-const renderMovieCards = (movies: Movie[]) => {
-  try {
-    const cards: string = movies
-      .map((m) => {
-        return `
-        <div class="movieCard">
-        <div class="movieImage">
-        <img src="${m.image}" />
-        </div>
-        
-        <div class="movieDetailsContainer">
-        <div class="movieDetails">
-        <h1 class="movieDetails__movieName"> ${m.name} </h1>
-        <h4 class="movieDetails__movieDescription"> ${m.description} </h4>
-        <p> Genre: ${m.genre} </p>
-        <p> Age Limit: ${m.ageLimit} </p>
-        <p> Premiere: ${m.premiere} </p>
-        <p> Duration in Minutes: ${m.screenDuration} </p>
-        </div>
-        </div>
-        </div>`;
-      })
-      .join(" ");
+let moviesArr: Movie[] = [];
 
-    movieCards.innerHTML = cards;
-  } catch (error) {
-    console.log(error);
+// Fetch movie data from JSON -
+fetch("movies.json")
+  .then((response) => response.json())
+  .then((data) => {
+    moviesArr = data;
+    renderMovieCards(moviesArr);
+    genreOptions();
+  })
+  .catch((error) => console.log(error));
+
+// Render movie cards -
+function renderMovieCards(movies: Movie[]) {
+  const movieCardsContainer = document.querySelector(".movieCards");
+  movieCardsContainer!.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const movieCard = document.createElement("div");
+    movieCard.classList.add("movieCard");
+
+    const movieImage = document.createElement("div");
+    movieImage.classList.add("movieImage");
+    const image = document.createElement("img");
+    image.src = movie.image;
+    movieImage.appendChild(image);
+
+    const movieDetails = document.createElement("div");
+    movieDetails.classList.add("movieDetails");
+
+    const movieName = document.createElement("h2");
+    movieName.classList.add("movieDetails__movieName");
+    movieName.textContent = movie.name;
+
+    const movieDescription = document.createElement("p");
+    movieDescription.classList.add("movieDetails__movieDescription");
+    movieDescription.textContent = movie.description;
+
+    const movieGenre = document.createElement("p");
+    movieGenre.classList.add("movieDetails__genre");
+    movieGenre.textContent = "Genre: " + movie.genre.join(", ");
+
+    const movieAgeLimit = document.createElement("p");
+    movieAgeLimit.classList.add("movieDetails__ageLimit");
+    movieAgeLimit.textContent = "Age Limit: " + movie.ageLimit;
+
+    const movieScreenDuration = document.createElement("p");
+    movieScreenDuration.classList.add("movieDetails__screenDuration");
+    movieScreenDuration.textContent =
+      "Screen Duration: " + movie.screenDuration;
+
+    const moviePremiere = document.createElement("p");
+    moviePremiere.classList.add("movieDetails__premiere");
+    moviePremiere.textContent = "Premiere: " + movie.premiere;
+
+    const moviePageButton = document.createElement("button");
+    moviePageButton.classList.add("movieDetails__moviePageButton");
+    moviePageButton.textContent = "Movie Page";
+    moviePageButton.addEventListener("click", () => {
+      window.open(movie.trailerURL, "_blank");
+    });
+
+    movieDetails.appendChild(movieName);
+    movieDetails.appendChild(movieDescription);
+    movieDetails.appendChild(movieGenre);
+    movieDetails.appendChild(movieAgeLimit);
+    movieDetails.appendChild(movieScreenDuration);
+    movieDetails.appendChild(moviePremiere);
+    movieDetails.appendChild(moviePageButton);
+
+    movieCard.appendChild(movieImage);
+    movieCard.appendChild(movieDetails);
+
+    movieCardsContainer!.appendChild(movieCard);
+  });
+}
+
+// Genre options -
+function genreOptions() {
+  const genreDropdown = document.getElementById("genreDropdown");
+
+  const allGenres = [
+    "action",
+    "kids",
+    "animation",
+    "comedy",
+    "crime",
+    "drama",
+    "sci-fi",
+    "horror",
+    "thriller",
+    "fantasy",
+    "musical",
+    "adventure",
+    "foreign",
+  ];
+
+  allGenres.forEach((genre) => {
+    const option = document.createElement("option");
+    option.value = genre;
+    option.textContent = genre;
+    genreDropdown!.appendChild(option);
+  });
+}
+
+// Handle genre change -
+function filterMoviesByGenre() {
+  const genreDropdown = document.getElementById(
+    "genreDropdown"
+  ) as HTMLSelectElement | null;
+
+  if (genreDropdown) {
+    const selectedGenre = genreDropdown.value;
+
+    if (selectedGenre === "") {
+      renderMovieCards(moviesArr);
+    } else {
+      const filteredMovies = moviesArr.filter((movie) =>
+        movie.genre.includes(selectedGenre)
+      );
+      renderMovieCards(filteredMovies);
+    }
   }
-};
-renderMovieCards(movies);
+}
+
+// Event listener for genre change -
+const genreDropdown = document.getElementById("genreDropdown");
+genreDropdown!.addEventListener("change", filterMoviesByGenre);
 /////////////////////////////////////////////////////////////////////////////////////
