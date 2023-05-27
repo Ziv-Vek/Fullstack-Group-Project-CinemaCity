64a330fa97fd51f203ad7df2c348d2f1d602e2b9
Ziv Vekstein <ziv.vek@gmail.com>
2023-05-27 10:23:56 +0300

    ziv auto compiler changes

--- dist/dist/view.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-// - Movie Cards - //
-var movieCards = document.querySelector(".movieCards"); // - Genre Dropdown - //
-
-var genreDropdown = document.getElementById("genreDropdown"); // (genreOptions function and filterMoviesByGenre function)
-
-var secondarySearchArea = document.querySelector(".search__secondary-search");
